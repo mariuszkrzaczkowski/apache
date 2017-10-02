aa0e7baf79120560b661661077fadc35b98239fa
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2017-10-02 12:15:26 +0200

    erg

--- test.php
+++ test.php
@@ -8,7 +8,7 @@
     <title>Bootstrap 101 Template <?php echo isset($_REQUEST['tttt'])?$_REQUEST['tttt']: '-';?></title>
   </head>
   <body>
-    <h1>Hello, world!@@</h1>
+    <h1>Hello, world!@@g</h1>
 	<a href="test.php?tttt=6666" class="menuIcon1"><span class="userIcon-Home"></span></a>
 	<a href="test.php?tttt=4444" class="menuIcon2"><span class="userIcon-Home"></span></a>
 	<a href="test.php?tttt=222" class="menuIcon3"><span class="userIcon-Home"></span></a>
